ddf198a73b09892d642e8b374dea92e5d3976f69
kamechan-lab <127114095+kamechan-lab@users.noreply.github.com>
2023-03-07 02:19:34 +0900

    Create PorlarCoordUtil.java

---
+++ FlexPieLib/utils/PorlarCoordUtil.java
@@ -0,0 +1,22 @@
+/**画面上の座標がDial中心から見て、どの角度・距離にあるのか計算するユーティリティクラスです。
+詳細は小西へ。
+**/
+public class PolarCoordUtil {
+    private PolarCoordUtil(){}
+
+    static float calcR(float x, float y, float ox, float oy){
+        return (float) Math.sqrt(Math.pow(x-ox, 2) + Math.pow(y-oy, 2));
+    }
+
+    static float calcDegrees(float x, float y, float ox, float oy) {
+        //内積(0, -1) * (x-ox, y-oy)
+        double inner_product = -(y - oy) / PolarCoordUtil.calcR(x, y, ox, oy);
+        double deg = Math.toDegrees(Math.acos(inner_product));
+        if ((x - ox) > 0) {
+            return (float) deg;
+        }
+        else {
+            return (float) (360 - deg);
+        }
+    }
+}
